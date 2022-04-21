351023571f17125c5e6a5f0584e5b2396225ebaa
MarcosRidley <MarcosRidley@hotmail.com>
2022-04-20 21:30:46 -0300

    finish async await setup

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // import fetch from 'node-fetch';
 
 function fetchAllPokemon() {
-	fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0')
+	fetch('https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0')
 		.then((response) => response.json())
 		.then((pokemonData) =>
-			pokemonData.results.forEach((pokemon) => fetchPokemon(pokemon.name))
+			pokemonData.results.forEach((pokemon) => fetchPokemon(pokemon.name) )
 		)
 		.catch((erro) => console.log('Ocorreu um erro no acesso a API', erro));
 }
 
-function fetchPokemon(nomePokemon) {
-	fetch(`https://pokeapi.co/api/v2/pokemon/${nomePokemon}`)
+async function fetchPokemon(nomePokemon) {
+	await fetch(`https://pokeapi.co/api/v2/pokemon/${nomePokemon}`)
 		.then((response) => response.json())
 		.then((pokemonData) => { createPokemonCard(pokemonData)})
 		.catch((erro) => console.log('Ocorreu um erro no acesso a API', erro));
 }
 
-async function createPokemonCard(pokemon) {
+ function createPokemonCard(pokemon) {
 	const cardContainer = document.querySelector('#container-cards');
 	const pokemonCard = document.createElement('div');
   pokemonCard.classList.add('cards')
@@ -35,21 +35,26 @@ async function createPokemonCard(pokemon) {
     } else {
       createTyping(pokemon.types[0].type.name, false, pokemonCard)
     }
+  } else {
+    createArceusTyping
   }
 	cardContainer.appendChild(pokemonCard);
 }
 
 function createTyping(type1, type2, card) {
 	const firstType = document.createElement('p');
+  const typeContainer = document.createElement('div')
+  typeContainer.classList.add('type-container')
 	firstType.textContent = type1;
-  firstType.classList.add(type1)
-	card.appendChild(firstType);
+  firstType.classList.add(type1, 'type')
+	typeContainer.appendChild(firstType);
 	if (type2) {
 		const secondType = document.createElement('p');
 		secondType.textContent = type2;
-    secondType.classList.add(type2)
-		card.appendChild(secondType);
+    secondType.classList.add(type2, 'type')
+		typeContainer.appendChild(secondType);
 	}
+  card.appendChild(typeContainer)
 }
 
 function createArceusTyping(card) {
@@ -64,7 +69,7 @@ function createArceusTyping(card) {
 // fetchPokemon('lucario');
 
 
-async function createAllPokeList() {
+function createAllPokeList() {
   try { fetchAllPokemon()
   } catch(error) {
     console.log("Ocorreu um erro: ", error)
